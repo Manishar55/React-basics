fe66a213e4ca5b4f1a2a4af675674b7e2d817a57
Manisha <manisharana02005@gmail.com>
2024-02-03 19:12:44 +0530

    Added example of pure react and babel script

---
+++ FirstReactProject/info.js
@@ -0,0 +1,36 @@
+// start setting up a new react project
+
+//write in the terminal:
+
+//1. npx create-react-app first-app (first-app app name)
+//2. cd first-app
+//3. npm start
+//4. http://localhost:3000    //pase it in browser
+
+//5. deleted all the files in public folder except index.html
+//6. deleted all the files in src folder except iApp.js and index.js
+
+//7. deleted everything from index.js except these:
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+//8. delete everything from App.js except these:
+
+// function App() {
+//     return (
+//       <div>
+//         Welcome to react App
+//       </div>
+//     );
+//   }
+  
+//   export default App;
