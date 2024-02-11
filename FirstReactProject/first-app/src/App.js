c984c5db229400c78986ffc23322145e97a4f7e4
Manisha <manisharana02005@gmail.com>
2024-02-11 11:25:02 +0530

    Added a simple vite project

--- FirstReactProject/first-app/src/App.js
+++ FirstReactProject/first-app/src/App.js
@@ -1,10 +1,22 @@
 
+// function App() {
+//   return (
+//     <div>
+//       <h1>Welcome to react App</h1>
+//     </div>
+//   );
+// }
+//creating a  functional component
+import React from 'react';
+
 function App() {
-  return (
-    <div>
-      <h1>Welcome to react App</h1>
-    </div>
-  );
+  return React.createElement(
+      'div',
+       {}, 
+       React.createElement('h1', {}, 'welcome to react'),
+       React.createElement('button', {}, 'click')
+       
+       );
 }
 
 export default App;
